0ee9d862ac2646c08920a267f4da9dfd78fb225c
Yannik Sander <contact@ysndr.de>
2017-07-01 01:16:00 +0200

    switched to using ViewHolders for SubstituteModels

--- app/src/main/java/de/ysndr/android/hgschedule/view/models/SubstituteModel.java
+++ app/src/main/java/de/ysndr/android/hgschedule/view/models/SubstituteModel.java
@@ -1,14 +1,16 @@
 package de.ysndr.android.hgschedule.view.models;
 
-import com.airbnb.epoxy.EpoxyAttribute;
-import com.airbnb.epoxy.EpoxyModel;
-import com.airbnb.epoxy.EpoxyModelClass;
+import android.view.View;
+import android.widget.TextView;
 
-import butterknife.ButterKnife;
-import butterknife.Unbinder;
+import com.airbnb.epoxy.EpoxyAttribute;
+import com.airbnb.epoxy.EpoxyModelClass;
+import com.airbnb.epoxy.EpoxyModelWithHolder;
+
+import butterknife.BindView;
 import de.ysndr.android.hgschedule.R;
 import de.ysndr.android.hgschedule.state.models.Substitute;
-import de.ysndr.android.hgschedule.view.views.SubstituteView;
+import de.ysndr.android.hgschedule.view.BaseEpoxyHolder;
 import timber.log.Timber;
 
 /**
@@ -16,7 +18,7 @@ import timber.log.Timber;
  */
 
 @EpoxyModelClass(layout = R.layout.model_substitute)
-public abstract class SubstituteModel extends EpoxyModel<SubstituteView> {
+public abstract class SubstituteModel extends EpoxyModelWithHolder<SubstituteModel.SubstituteHolder> {
 
     @EpoxyAttribute
     Substitute substitute;
@@ -36,36 +38,46 @@ public abstract class SubstituteModel extends EpoxyModel<SubstituteView> {
 
         @BindView(R.id.text_room_card_schedule_item)
         TextView room;*/
-    Unbinder unbinder;
 
     @Override
-    public void bind(SubstituteView view) {
-        super.bind(view);
-        unbinder = ButterKnife.bind(this, view);
-
+    public void bind(SubstituteHolder holder) {
+        super.bind(holder);
         Timber.d("in `bind()`");
 
-        view.setAbsent(substitute.absent());
-        view.setSubst(substitute.substitute());
-        view.setClasses(substitute.classes());
-        view.setLessons(substitute.hour());
-        view.setDescription(substitute.description());
-        view.setRoom(substitute.room());
-        view.setTitle(substitute.absent());
+        holder.absent.setText(substitute.absent());
+        holder.subst.setText(substitute.substitute());
+        holder.classes.setText(substitute.classes());
+        holder.lessons.setText(substitute.hour());
+        holder.description.setText(substitute.description());
+        holder.room.setText(substitute.room());
+        holder.title.setText(substitute.absent());
         // TODO maybe change to a visible/invisible filtering
     }
 
-    @Override
-    public void unbind(SubstituteView view) {
-        super.unbind(view);
 
-        Timber.d("in `unbind(...)`");
-        unbinder.unbind();
+    static class SubstituteHolder extends BaseEpoxyHolder {
+        @BindView(R.id.text_title_card_schedule_item)
+        TextView title;
+        @BindView(R.id.text_description_card_schedule_item)
+        TextView description;
+        @BindView(R.id.text_teacher_absent_card_schedule_item)
+        TextView absent;
+        @BindView(R.id.text_teacher_new_card_schedule_item)
+        TextView subst;
+        @BindView(R.id.text_lessons_card_schedule_item)
+        TextView lessons;
+        @BindView(R.id.text_classes_card_schedule_item)
+        TextView classes;
+
+        @BindView(R.id.text_room_card_schedule_item)
+        TextView room;
+
+        @Override
+        protected void bindView(View itemView) {
+            super.bindView(itemView);
+            Timber.d("binding view to SubstituteHolder");
+        }
     }
 
-    @Override
-    public void onViewDetachedFromWindow(SubstituteView view) {
-        super.onViewDetachedFromWindow(view);
-        Timber.d("in `detatch`");
-    }
+
 }
